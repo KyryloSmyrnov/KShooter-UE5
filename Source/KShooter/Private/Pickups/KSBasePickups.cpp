1538b684cab4319196b74958e8d4bedb33969388
Kyrylo Smyrnov <94990448+KyryloSmyrnov@users.noreply.github.com>
2022-05-24 18:26:44 +0200

    Pickups rotating

--- Source/KShooter/Private/Pickups/KSBasePickups.cpp
+++ Source/KShooter/Private/Pickups/KSBasePickups.cpp
@@ -22,11 +22,14 @@ void AKSBasePickups::BeginPlay()
     Super::BeginPlay();
 
     check(CollisionComponent);
+    GenerateRotationYaw();
 }
 
 void AKSBasePickups::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 void AKSBasePickups::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -58,9 +61,17 @@ void AKSBasePickups::PickupWasTaken()
 
 void AKSBasePickups::Respawn()
 {
+    GenerateRotationYaw();
+    
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
     if(GetRootComponent())
     {
         GetRootComponent()->SetVisibility(true, true);
     }
+}
+
+void AKSBasePickups::GenerateRotationYaw()
+{
+    const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
